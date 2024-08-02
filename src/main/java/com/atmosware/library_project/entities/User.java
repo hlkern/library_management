4ad39ea481f971db871c84873cb1998d4508b98b
Aziz Alp Yolcu <azizalpyolcu09@outlook.com>
2024-08-02 11:42:05 +0300

    2.08.24

---
+++ src/main/java/com/atmosware/library_project/entities/User.java
@@ -0,0 +1,4 @@
+package com.atmosware.library_project.entities;
+
+public class User {
+}
---
+++ src/test/java/com/atmosware/library_project/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmosware.library_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
