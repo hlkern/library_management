e3fc68f341f14ba37e15c3b035cc9ab86b717c91
hlkern <halukerenfurtuna@gmail.com>
2024-08-28 23:05:38 +0300

    fix: corrected typo

--- src/main/java/com/atmosware/library_project/business/abstracts/NotificationService.java
+++ src/main/java/com/atmosware/library_project/business/abstracts/NotificationService.java
@@ -3,6 +3,6 @@ package com.atmosware.library_project.business.abstracts;
 import java.util.List;
 
 public interface NotificationService {
-    void sendNotificationToAllUsers(List<String> userEmails, String bookTıtle, String bookAuthor);
+    void sendNotificationToAllUsers(List<String> userEmails, String bookTitle, String bookAuthor);
     void sendNotification(String to, String subject, String body);
 }
