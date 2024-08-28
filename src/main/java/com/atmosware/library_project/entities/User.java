820aefdc71d76eec1da09d8f9c0eb24833191286
hlkern <halukerenfurtuna@gmail.com>
2024-08-28 22:17:18 +0300

    fix

--- src/main/java/com/atmosware/library_project/entities/User.java
+++ src/main/java/com/atmosware/library_project/entities/User.java
@@ -41,7 +41,7 @@ public class User extends BaseEntity implements UserDetails {
 
     @Override
     public String getUsername() {
-        return email;
+        return username;
     }
 
     @Override
